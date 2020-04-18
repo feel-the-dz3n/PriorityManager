4e55af93e7a84f230feddd752c9592a486ac94f2
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-19 00:51:00 +0300

    NotifyPriorityChangeFailed: show error message

--- PriorityManager/notifications.cpp
+++ PriorityManager/notifications.cpp
@@ -29,8 +29,12 @@ VOID NotifyPriorityChangeFailed(LPCWSTR target, DWORD dwPriorityClass, BOOL bGet
 
 	if (bGetLastError) 
 	{
-		DWORD lastError = GetLastError();
-		swprintf_s(buf, L"%s (%d).", buf, lastError); // Get error string here
+		DWORD lastError = GetLastError(); 
+		wchar_t errorBuf[256];
+		FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL, lastError, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			errorBuf, (sizeof(errorBuf) / sizeof(wchar_t)), NULL);
+		swprintf_s(buf, L"%s %s (code: %d).", buf, errorBuf, lastError); // Get error string here
 	}
 
 	ShowTrayBalloon(buf, L"Failed to set priority", 1500);
