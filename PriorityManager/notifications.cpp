850bdad5c5e68ab54a38db6000a487d91acd7590
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-04-19 00:44:37 +0300

    Add NotifyPriorityChangeFailed

--- PriorityManager/notifications.cpp
+++ PriorityManager/notifications.cpp
@@ -19,5 +19,19 @@ VOID NotifyPriorityChanged(LPCWSTR target, DWORD dwPriorityClass)
 {
 	TCHAR buf[1024];
 	swprintf_s(buf, L"%s priority for %s", GetPriorityText(dwPriorityClass), target);
-	ShowTrayBalloon(buf, L"Priority Changed", 1500);
+	ShowTrayBalloon(buf, L"Priority changed", 1500);
+}
+
+VOID NotifyPriorityChangeFailed(LPCWSTR target, DWORD dwPriorityClass, BOOL bGetLastError)
+{
+	TCHAR buf[1024];
+	swprintf_s(buf, L"Unable to set priority for %s.", target);
+
+	if (bGetLastError) 
+	{
+		DWORD lastError = GetLastError();
+		swprintf_s(buf, L"%s (%d).", buf, lastError); // Get error string here
+	}
+
+	ShowTrayBalloon(buf, L"Failed to set priority", 1500);
 }
